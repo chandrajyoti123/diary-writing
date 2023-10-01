811239fb7f59ae81e81cb979ca97f53c9bb2ac44
chandrajyoti123 <chandrajyotiadil15062003@gmail.com>
2023-10-01 22:10:46 +0530

    initial commit

---
+++ src/components/SweetsCard/SweetsCard.js
@@ -0,0 +1,6 @@
+const SweetsCard=()=>{
+    return(
+        <h1>this is sweetscard</h1>
+    )
+}
+export default SweetsCard;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-
 import ReactDOM from 'react-dom/client';
 import Home from './view/Home/Home';
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Home/>
-
+<Home/>
 );
-
